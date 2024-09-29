6682d0144f79d9f6016b058a30ffd21bf7d12382
Jordan Gardner <jordanthomasg@gmail.com>
2024-09-29 16:12:00 -0600

    Add url table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,69 @@
+"use client";
+
+import ShortenedUrlsTable from "@/components/ShortendUrlsTable";
+import { shortenedUrls } from "./database";
+import { ChangeEventHandler, useState } from "react";
+
 export default function Home() {
+  const [url, setUrl] = useState("");
+  const [editing, setEditing] = useState(false);
+
+  let urlIsValid;
+  try {
+    const { protocol } = new URL(url);
+    urlIsValid = ["http:", "https:"].includes(protocol);
+  } catch {
+    urlIsValid = false;
+  }
+
+  const showError = url && !editing && !urlIsValid;
+  const canSubmit = url && urlIsValid;
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setUrl(event.target.value);
+  };
+
+  const handleFocus = () => {
+    setEditing(true);
+  };
+
+  const handleBlur = () => {
+    setEditing(false);
+  };
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <form>
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start mx-auto">
+        <form className="w-full">
           <div className="flex flex-col gap-2 items-center">
             <input
               id="url"
               name="url"
               type="url"
               placeholder="Enter your URL here..."
+              value={url}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
               className="block w-full rounded-full border-0 px-4 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 leading-6 sm:min-w-96 lg:min-w-lg"
             />
+            {url && !editing && !urlIsValid && (
+              <p className="text-sm text-red-600">Please enter a valid URL</p>
+            )}
             <div className="flex gap-4 items-center flex-col sm:flex-row">
               <button
                 type="button"
-                className="rounded-full bg-indigo-600 px-4 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                disabled={!canSubmit}
+                className="rounded-full bg-indigo-600 px-4 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:bg-gray-400"
               >
                 Shorten URL
               </button>
             </div>
           </div>
         </form>
+        <div className="flex flex-col gap-4 border border-solid border-gray-300 rounded-lg w-full">
+          <ShortenedUrlsTable urls={Object.values(shortenedUrls)} />
+        </div>
       </main>
     </div>
   );
---
+++ src/components/ShortendUrlsTable.tsx
@@ -0,0 +1,83 @@
+import type { ShortenedUrl } from "@/app/database";
+
+export default function ShortenedUrlsTable({ urls }: { urls: ShortenedUrl[] }) {
+  return (
+    <div className="p-4">
+      <div className="flow-root">
+        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+            <table className="min-w-full divide-y divide-gray-300">
+              <thead>
+                <tr>
+                  <th
+                    scope="col"
+                    className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0 hidden md:table-cell"
+                  >
+                    Slug
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Short URL
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 hidden md:table-cell"
+                  >
+                    Long URL
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 hidden md:table-cell"
+                  >
+                    Redirects
+                  </th>
+                  <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
+                    <span className="sr-only">Delete</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-gray-200">
+                {urls.map(({ slug, url, redirects }) => (
+                  <tr key={slug}>
+                    <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0 hidden md:table-cell">
+                      {slug}
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm font-medium sm:pr-0">
+                      <a
+                        href={`/${slug}`}
+                        className="text-indigo-600 hover:text-indigo-900"
+                      >
+                        {`http://localhost:3000/${slug}`}
+                      </a>
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm font-medium sm:pr-0 hidden md:table-cell">
+                      <a
+                        href={`/${slug}`}
+                        className="text-indigo-600 hover:text-indigo-900"
+                      >
+                        {url}
+                      </a>
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500 hidden md:table-cell">
+                      {redirects}
+                    </td>
+                    <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                      <a
+                        href="#"
+                        className="text-indigo-600 hover:text-indigo-900"
+                      >
+                        Delete
+                      </a>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
